4a53b2acfa2ce52b41f65d939bcb0956b77fcd67
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-19 11:49:25 +0200

    init

---
+++ OrchardClash/App/OrchardClashApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrchardClashApp.swift
+//  OrchardClash
+//
+//  Created by Serhii Babchuk on 19.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct OrchardClashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OCLaunchView()
+        }
+    }
+}
